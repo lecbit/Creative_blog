b26852e5eb36a91f740fc9db9bed7eebbda3d22e
lecbit <artm-marych@rambler.ru>
2022-10-27 21:10:35 +0200

    ~19~ Editing of posts

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -15,8 +15,8 @@ class StoreController extends Controller
     {
         try {
             $data = $request->validated();
-            $data['preview_image'] = Storage::put('/images', $request['preview_image']);
-            $data['main_image'] = Storage::put('/images', $request['main_image']);
+            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
 
             $tag_ids = $data['tag_ids'];
             unset($data['tag_ids']);
